65d130ee23eb2ab981ee5c9eb7eba86282ae0577
andrew pate <andrewpate08@hotmail.com>
2020-11-26 13:20:30 +0000

    Improve granularity of population data from 2011 UK survey

--- population_creator.py
+++ population_creator.py
@@ -4,7 +4,12 @@ from person import Person
 class PopulationCreator(object):
 
     def __init__(self):
-        self.age_dist=[7431000, 7667000, 8604000, 8320000, 9269000, 7709000, 6824000, 4469000, 2414000, 476000] # population in decade groups
+        #self.age_dist=[7431000, 7667000, 8604000, 8320000, 9269000, 7709000, 6824000, 4469000, 2414000, 476000] # population in decade groups
+        self.age_dist=[3914000,3517000,3670000, \
+        3997000,4297000,4307000,4126000,4194000, \
+        4626000,4643000,4095000,3614000,3807000, \
+        3017000,2463000,2006000,1496000,918000,476000] # population in 5 year groups
+        
         self.male_prob = 0.5
         self.adhd_prob = 0.02
         self.depressed_prob = 0.03 # mind severe depression
@@ -18,11 +23,11 @@ class PopulationCreator(object):
         pop = sum(self.age_dist)
         people = []
         
-        for x in range(9,-1,-1):
+        for x in range(18,-1,-1):
             age_seg = int(self.age_dist[x] * size / pop)
             for dood in range(0, age_seg):
                 person = Person()
-                person.age = x * 10 + 5 # could be more random here
+                person.age = x * 5 + 2 # could be more random here
                 if random.random() < self.male_prob:
                     person.male = True
                 if random.random() < self.adhd_prob:
